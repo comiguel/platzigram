206f99780514e6f04ea8cb46f93bcd53aef8e988
Carlos Hincapie <carmihinfer@gmail.com>
2016-07-20 17:34:37 -0500

    Añadiendo funcionalidad de la cámara

---
+++ src/homepage/index.js
@@ -0,0 +1,114 @@
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+var title = require('title');
+var request = require('superagent');
+var header = require('../header');
+var axios = require('axios');
+var Webcam = require('webcamjs');
+var picture = require('../picture-card');
+
+page('/', header, asyncLoad, function (ctx, next) {
+  title('Platzigram');
+  var main = document.getElementById('main-container');
+
+  empty(main).appendChild(template(ctx.pictures));
+
+  const picturePreview = $('#picture-preview');
+  const camaraInput = $('#camara-input');
+  const cancelPicture = $('#cancel-picture');
+  const shootButton = $('#shoot');
+  const uploadButton = $('#uploadButton');
+
+  function reset(){
+    picturePreview.addClass('hide');
+    cancelPicture.addClass('hide');
+    uploadButton.addClass('hide');
+    shootButton.removeClass('hide');
+    cameraInput.removeClass('hide');
+  }
+
+  cancelPicture.click(reset);
+
+  $('.modal-trigger').leanModal({
+    ready: function(){
+      Webcam.attach('#camara-input');
+      shootButton.click(ev => {
+        Webcam.snap(data_uri => {
+          picturePreview.html(`<img src="${data_uri}"/>`);
+          picturePreview.removeClass('hide');
+          cancelPicture.removeClass('hide');
+          uploadButton.removeClass('hide');
+          shootButton.addClass('hide');
+          cameraInput.addClass('hide');
+          uploadButton.click(() => {
+            const pic = {
+              url: data_uri,
+              likes: 0,
+              liked: false,
+              createdAt: +new Date(),
+              user: {
+                username: 'comiguel0812',
+                avatar: 'https://scontent.fgig1-4.fna.fbcdn.net/v/t1.0-1/c42.42.528.528/s160x160/1234142_10151869965756095_1675162330_n.jpg?oh=5458f8d2ee5f0aa83e4ba0eb57379258&oe=58345EDF'
+              }
+            }
+            $('#picture-cards').prepend(picture(pic));
+            reset();
+            $('#modalCamara').closeModal();
+
+          })
+        })
+      });
+    },
+    complete: function(){
+      Webcam.reset();
+      reset();
+    }
+  });
+})
+
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function(err, res){
+      if (err) return console.log(err);
+
+      ctx.pictures = res.body;
+      next();
+    });
+}
+
+function loadPicturesAxios(ctx, next) {
+  axios
+    .get('/api/pictures')
+    .then(function(res){
+      ctx.pictures = res.data;
+      next();
+    })
+    .catch(function(err){
+      console.log(err)
+    });
+}
+
+function loadPicturesFetch(ctx, next) {
+  fetch('/api/pictures')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (pictures){
+      ctx.pictures = pictures;
+      next();
+    })
+    .catch(function(err){
+      console.log(err)
+    });
+}
+
+async function asyncLoad(ctx, next){
+  try {
+    ctx.pictures = await fetch('/api/pictures').then(res => res.json());
+    next();
+  } catch (err) {
+    return console.log(err);
+  }
+}
